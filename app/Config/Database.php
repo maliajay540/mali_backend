ed606a04e089213ddd2aaeebdfb5d4432bb578ca
maliajay540 <ajayganeshmali@gmail.com>
2026-03-10 16:42:07 +0530

    connect railway mysql

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,31 +25,21 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'DSN'      => '',
-    'hostname' => 'localhost',
+
+    'DSN'      => '',
+    'hostname' => 'witchyard.proxy.rlwy.net',
     'username' => 'root',
-    'password' => '',
-    'database' => 'mali_parivar',
+    'password' => 'IMgVwkzcVoBIgrqkcomOZhTMdrYaLqXY',
+    'database' => 'railway',
     'DBDriver' => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+    'DBPrefix' => '',
+    'pConnect' => false,
+    'DBDebug'  => true,
+    'charset'  => 'utf8',
+    'DBCollat' => 'utf8_general_ci',
+    'port'     => 28301,
+];
+
 
     //    /**
     //     * Sample database connection for SQLite3.
