6ca87d635e805d1a56c91c69050ef7b89e0fcd10
maliajay540 <ajayganeshmali@gmail.com>
2026-03-24 01:15:07 +0530

    save current wor21k

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -76,6 +76,7 @@ class Filters extends BaseFilters
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
+            'cors',
         ],
         'after' => [
             // 'honeypot',
